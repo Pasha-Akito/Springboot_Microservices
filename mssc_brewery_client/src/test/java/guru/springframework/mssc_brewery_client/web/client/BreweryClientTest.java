6bb94f9e70d5f3b2fa2d6e4df8e8bb9c3fa55f88
Pasha-Akito <1masterpasha@gmail.com>
2022-06-27 10:21:15 +0100

    Client Customer CRUD Tests implemented. Closes #2

--- mssc_brewery_client/src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
+++ mssc_brewery_client/src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.mssc_brewery_client.web.client;
 
 import guru.springframework.mssc_brewery_client.web.model.BeerDto;
+import guru.springframework.mssc_brewery_client.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -49,6 +50,36 @@ class BreweryClientTest {
     @Test
     void deleteBeer(){
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById(){
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer(){
+        CustomerDto customerDto = CustomerDto.builder()
+                .customerName("Pasha")
+                .build();
+
+        URI uri = client.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer(){
+        CustomerDto customerDto = CustomerDto.builder()
+                .customerName("Pasha")
+                .build();
+
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer(){
+        client.deleteCustomer(UUID.randomUUID());
     }
 
 }
