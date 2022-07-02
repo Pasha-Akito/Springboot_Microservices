c4205d1a0e3c76c5900393f9ca8f0729aaa3bd0c
Pasha-Akito <1masterpasha@gmail.com>
2022-07-02 12:56:37 +0100

    Updated Tests in service

--- mssc_brewery_service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ mssc_brewery_service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,21 +1,29 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "guru.springframework.msscbeerservice.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -24,8 +32,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
@@ -54,11 +67,11 @@ class BeerControllerTest {
 
     BeerDto getValidBeerDto(){
         return BeerDto.builder()
-                .beerName("BeerName")
+                .beerName("Nice Ale")
                 .beerStyle(BeerStyleEnum.ALE)
-                .quantityOnHand(1)
-                .price(new BigDecimal("1"))
-                .upc(1L)
+                .quantityOnHand(10)
+                .price(new BigDecimal("9.99"))
+                .upc(123123123123L)
                 .build();
     }
 }
